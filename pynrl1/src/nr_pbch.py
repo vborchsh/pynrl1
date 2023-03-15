8aa0410262561921cdf60b1015bac81812755741
Vladislav Borshch <borchsh.vn@gmail.com>
2023-03-15 12:05:02 +0700

    [feature] PBCH indices

--- pynrl1/src/nr_pbch.py
+++ pynrl1/src/nr_pbch.py
@@ -18,5 +18,28 @@ def nr_pbch(ncellid, v, databits):
     prbs_bits = prbs_bits[-n:]
     scrambled_bits = np.bitwise_xor(databits, prbs_bits)
 
+    # Indices with windows for SSB
+    K = 240
+    ind  = list(range(  K,        2*K, 4)) #l = 1, k =   0 ... 236
+    ind += list(range(2*K,     2*K+48, 4)) #l = 2, k =   0 ...  44
+    ind += list(range(2*K+192,    3*K, 4)) #l = 2, k = 192 ... 236
+    ind += list(range(3*K,        4*K, 4)) #l = 3, k =   0 ... 236
+
+    # 3 indices for PBCH within groups of 4, depending on DMRS shift
+    v_indices = ncellid % 4
+    if v_indices == 0:
+        dmrs_ind = [1, 2, 3]
+    elif v_indices == 1:
+        dmrs_ind = [0, 2, 3]
+    elif v_indices == 2:
+        dmrs_ind = [0, 1, 3]
+    elif v_indices == 3:
+        dmrs_ind = [0, 1, 2]
+
+    # Compile DMRS+PBCH indices
+    res_ind = [j for x in ind for j in [x+dmrs_ind[0], x+dmrs_ind[1], x+dmrs_ind[2]]]
+
     # Modulation, Section 7.3.3.2, TS 38.211
-    return nr_mapper(scrambled_bits, 'QPSK')
+    res_sym = nr_mapper(scrambled_bits, 'QPSK')
+
+    return np.array(res_ind), np.array(res_sym)
--- tests/test_nr_pbch.py
+++ tests/test_nr_pbch.py
@@ -7,14 +7,20 @@ from pynrl1.src.nr_pbch import nr_pbch
 
 def run_nr_pbch(ncellid, databits, v, eng):
     ref_data = eng.nrPBCH(databits, matlab.double(ncellid), matlab.double(v), nargout=1)
+    ref_indices = eng.nrPBCHIndices(ncellid);
+
     ref_data = np.array(list(itertools.chain(*ref_data)))
 
-    data = nr_pbch(ncellid, v, np.array(list(itertools.chain(*databits)), dtype=(int)))
+    ref_indices = np.array(list(itertools.chain(*ref_indices)))
+    ref_indices = [x-1 for x in ref_indices]
+
+    indices, data = nr_pbch( ncellid, v, np.array(list(itertools.chain(*databits)), dtype=(int)) )
 
     ref_data = np.around(ref_data, 4)
     data = np.around(data, 4)
 
     assert (ref_data == data).all()
+    assert (ref_indices == indices).all()
 
 @pytest.mark.parametrize("ncellid", [1, 500, 1007])
 @pytest.mark.parametrize("v", list(range(8)))
