e7cee0096b659b34903d299f1315bbe6a5a30a00
Vladislav Borshch <borchsh.vn@gmail.com>
2023-04-17 15:34:35 +0700

    [tests] Tests for PDSCH data symbols and indices

--- tests/test_nr_pdschdmrs.py
+++ tests/test_nr_pdschdmrs.py
@@ -3,10 +3,17 @@ import itertools
 import numpy as np
 import pytest
 
-from pynrl1.downlink.nr_pdschdmrs_indices import nr_pdschdmrs_indices
+from pynrl1.downlink.nr_pdschdmrs import nr_pdschdmrs
 from pynrl1.util.configurations import nrPDSCH_config
+from pynrl1.util.configurations import nrCarrier_config
 
-def run_nr_pdschdmrs_indices(cfg, eng):
+def run_nr_pdschdmrs(cfg, eng):
+    carrier = nrCarrier_config();
+    carrier.subcarrier_spacing = 120;
+    carrier.cyclic_prefix = 'normal';
+    carrier.n_size_grid = 132;
+    carrier.n_start_grid = 0;
+
     pdsch_cfg = nrPDSCH_config();
     pdsch_cfg.n_size_bwp = cfg['n_size_bwp']
     pdsch_cfg.n_start_bwp = cfg['n_start_bwp']
@@ -20,19 +27,23 @@ def run_nr_pdschdmrs_indices(cfg, eng):
     pdsch_cfg.PRB_set = cfg['PRBSet']
     pdsch_cfg.symbol_allocation = cfg['SymbolAllocation']
 
-    pdschdmrs_indices = nr_pdschdmrs_indices(pdsch_cfg)
-    [pdschdmrs_syms_ref, pdschdmrs_indices_ref] = eng.gen_pdschdmrs(cfg, nargout=2)
-    pdschdmrs_indices_ref = np.array(list(itertools.chain(*pdschdmrs_indices_ref))) - 1
+    pdschdmrs_syms = nr_pdschdmrs(pdsch_cfg, carrier)
 
-    assert np.array_equal(pdschdmrs_indices, pdschdmrs_indices_ref)
+    [pdschdmrs_syms_ref, pdschdmrs_indices_ref] = eng.gen_pdschdmrs(cfg, nargout=2)
+    pdschdmrs_syms_ref = np.array(list(itertools.chain(*pdschdmrs_syms_ref)))
+
+    pdschdmrs_syms = np.around(pdschdmrs_syms, 4)
+    pdschdmrs_syms_ref = np.around(pdschdmrs_syms_ref, 4)
+
+    assert np.array_equal(pdschdmrs_syms, pdschdmrs_syms_ref)
 
 
 @pytest.mark.parametrize('typeA_pos', [2, 3])
 @pytest.mark.parametrize('symb_alloc', [[2, 12]])
 @pytest.mark.parametrize('dmrs_add_pos', [0, 1, 2, 3])
-@pytest.mark.parametrize('PRBSet', [list(range(0, 132)), list(range(60, 132)), list(range(0, 60))])
+@pytest.mark.parametrize('PRBSet', [list(range(0, 132)), list(range(60, 132)), list(range(30, 60))])
 @pytest.mark.parametrize('dmrs_cfg_type', [1, 2])
-def test_nr_pdschdmrs_indices(symb_alloc, dmrs_add_pos, typeA_pos, PRBSet, dmrs_cfg_type):
+def test_nr_pdschdmrs(symb_alloc, dmrs_add_pos, typeA_pos, PRBSet, dmrs_cfg_type):
     eng = matlab.engine.connect_matlab()
 
     cfg = {}
@@ -49,9 +60,9 @@ def test_nr_pdschdmrs_indices(symb_alloc, dmrs_add_pos, typeA_pos, PRBSet, dmrs_
     cfg['NSCID'] = 0
 
     try:
-        run_nr_pdschdmrs_indices(cfg, eng)
+        run_nr_pdschdmrs(cfg, eng)
     finally:
         eng.quit()
 
 if __name__ == '__main__':
-    test_nr_pdschdmrs_indices([2, 12], 1, 2, list(range(2, 130)), 1)
+    test_nr_pdschdmrs([2, 12], 1, 2, list(range(2, 130)), 2)
